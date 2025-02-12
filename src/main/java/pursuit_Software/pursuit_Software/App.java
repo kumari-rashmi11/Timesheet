7384c47811a369bbd0ba23c15631bf010a9d95ff
Rashmi Kumari <rashmi_k@pursuitsoftware.com>
2025-02-12 11:31:01 +0530

    pursuit code

---
+++ src/main/java/pursuit_Software/pursuit_Software/App.java
@@ -0,0 +1,10 @@
+package pursuit_Software.pursuit_Software;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
