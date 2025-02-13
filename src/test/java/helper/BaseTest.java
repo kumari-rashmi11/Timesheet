e049e359e1753e7f0d30eeb4602a101ec37f47e3
kumari-rashmi11 <rashmi_k@pursuitsoftware.com>
2025-02-13 14:29:05 +0530

    Update BaseTest.java

--- src/test/java/helper/BaseTest.java
+++ src/test/java/helper/BaseTest.java
@@ -7,7 +7,6 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 
@@ -27,7 +26,7 @@ public class BaseTest {
     
     @BeforeMethod
     public void setup() {
-    	driver = new EdgeDriver();
+    	driver = new ChromeDriver();
     	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     	loadProperties();
     	driver.get(properties.getProperty("URL"));
