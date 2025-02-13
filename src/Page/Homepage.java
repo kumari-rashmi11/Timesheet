3c7302aebe20fad158ad1ded651e22377088c6a7
Rashmi Kumari <rashmi_k@pursuitsoftware.com>
2025-02-13 11:06:43 +0530

    updated code

--- src/Page/Homepage.java
+++ src/Page/Homepage.java
@@ -4,5 +4,9 @@ public class Homepage {
 	 public void printMessage() {
 	        System.out.println("Hello, World!");
 	    }
+	 
+	 public void displayWelcome() {
+	        System.out.println("Welcome to Automation Testing!");
+	    }
 	}
 
--- src/Test/Hometest.java
+++ src/Test/Hometest.java
@@ -1,17 +1,36 @@
 package Test;
- 
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import Page.Homepage;
- 
- 
 public class Hometest {
     
-    @Test(priority = 1)
-    public void printMessage() {
-    	Homepage obj = new Homepage();
-        obj.printMessage();
-    }
-}
+    protected WebDriver driver;
+    protected WebDriverWait wait;
 
- 
+    @BeforeMethod
+    public void setup() {
+//        System.setProperty("webdriver.edge.driver", "C:\\WebDrivers\\msedgedriver.exe"); // Set the correct path
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+
+    @Test(priority = 1)
+    public void openGooglePage() {
+        driver.get("https://www.google.com/");
+        System.out.println("Page title is: " + driver.getTitle());
+    }
+
+	 @AfterMethod
+		public void afterMethod() throws InterruptedException {
+			Thread.sleep(2000);
+			driver.quit();
+	 }    
+}
